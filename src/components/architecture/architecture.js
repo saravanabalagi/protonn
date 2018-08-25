abfad0de13353148c0333f34f3d6868d91d646ee
Saravanabalagi R <saravanabalagi@hotmail.com>
2018-08-25 18:31:32 +0100

    Adding buttons to architecture

--- src/components/architecture/architecture.js
+++ src/components/architecture/architecture.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import {Button, Control, Field, Label, Select} from "bloomer";
 
 class Architecture extends Component {
   render() {
     return (
       <div>
-        Architecture
+        <Field>
+          <Label>Add Layer:</Label>
+          <div className="buttons has-addons">
+            <Button className="is-success is-selected">Dense</Button>
+            <Button>Conv2D</Button>
+          </div>
+          <Button isColor='info'>Add Layer</Button>
+        </Field>
       </div>
     );
   }
