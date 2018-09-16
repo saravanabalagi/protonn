6161df7da6219295e588e4ecb00e483c69671bbc
Saravanabalagi R <saravanabalagi@hotmail.com>
2018-09-16 11:09:30 +0100

    Fixing upSampleSize and poolSize changes

--- src/reducers/layer/maxPooling2dActions.js
+++ src/reducers/layer/maxPooling2dActions.js
@@ -2,6 +2,6 @@ export const CHANGE_POOL_SIZE = "CHANGE_POOL_SIZE";
 
 export function changePoolSize(layerPosition, poolSize) {
   return (dispatch) => {
-    dispatch({ type: CHANGE_POOL_SIZE, upSampleSize: poolSize})
+    dispatch({ type: CHANGE_POOL_SIZE, upSampleSize: poolSize, layerPosition: layerPosition })
   }
 }
--- src/reducers/layer/upSampling2dActions.js
+++ src/reducers/layer/upSampling2dActions.js
@@ -1,7 +1,7 @@
 export const CHANGE_POOL_SIZE = "CHANGE_POOL_SIZE";
 
-export function changeUpSampleSize(layerPosition, poolSize) {
+export function changeUpSampleSize(layerPosition, upSampleSize) {
   return (dispatch) => {
-    dispatch({ type: CHANGE_POOL_SIZE, upSampleSize: poolSize})
+    dispatch({ type: CHANGE_POOL_SIZE, upSampleSize: upSampleSize, layerPosition: layerPosition })
   }
 }
